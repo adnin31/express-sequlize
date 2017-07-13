a871e25fb0454616e2ab7db70a48a9b56f2fc121
adnin31 <adnin.ris31@gmail.com>
2017-07-13 17:24:53 +0700

    selesai sampai nomor 5

---
+++ models/teacher.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Teacher = sequelize.define('Teacher', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Teacher;
+};
